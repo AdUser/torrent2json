0108200b8c48391aa04f5f4602a05dfe617f0a74
Alex 'AdUser' Z <ad_user@mail.ru>
2011-09-20 21:15:25 +1100

    + added code skeleton for json2torrent

---
+++ src/json2torrent.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <yajl/yajl_parse.h>
+
+/* FD's */
+FILE *in;
+FILE *out;
+
+int
+main(int argc, char **argv)
+{
+  /* FILE */ in  = stdin;
+  /* FILE */ out = stdout;
+
+  exit(EXIT_SUCCESS);
+}
+
